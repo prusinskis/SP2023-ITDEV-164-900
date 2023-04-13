1475cf8df02fd1ef61c5662bccc5131754e2c6f0
Sherise Prusinski <98294721+prusinskis@users.noreply.github.com>
2023-04-13 14:12:20 -0500

    Create and Use the List components

--- blog/src/components/BaseContainer/BaseContainer.js
+++ blog/src/components/BaseContainer/BaseContainer.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { Flex, Box } from 'rebass'
 
 export const BaseContainer = ({ children, flex = false, ... rest}) => {
-    let component = flex ? Flex : Box
+    let Component = flex ? Flex : Box
     return <Component {...rest}> { children} </Component>
 }
 
--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -4,15 +4,16 @@ import { GatsbyImage} from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { List, ListItem } from '../components/List'
 import * as styles from "../components/index.module.css"
 
 const IndexPage = ( {data}) => (
   <Layout>
     <Seo title="Home" />
-    <ul className={styles.list}>
+    <List width={1, 2/3,7/8} p={2}>
       {
         data.allContentfulBlogPost.edges.map(edge => (
-          <li key={edge.node.id}>
+          <ListItem p={3} key={edge.node.id}>
             <Link to={edge.node.slug}>{edge.node.title}</Link>
             <div>
               <GatsbyImage
@@ -22,10 +23,10 @@ const IndexPage = ( {data}) => (
             <div>
               {edge.node.body.childMarkdownRemark.excerpt}
             </div>
-          </li>
+          </ListItem>
         ))
       }
-    </ul>
+    </List>
   </Layout>
 )
 
