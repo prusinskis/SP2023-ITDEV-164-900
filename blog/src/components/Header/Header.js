904a6351c7288725f344ff66b95c75dc8f46fc63
Sherise Prusinski <98294721+prusinskis@users.noreply.github.com>
2023-05-28 13:00:22 -0500

    gatsby updated to 5.10

--- blog/src/components/Header/Header.js
+++ blog/src/components/Header/Header.js
@@ -1,5 +1,4 @@
 import * as React from "react"
-import PropTypes from "prop-types"
 import {Link} from "gatsby"
 import styled, { ThemeConsumer } from 'styled-components'
 import { Flex } from 'rebass'
